5d4f77939ed4082ca049f76c078dfa3c546f99bd
Kunihiro Narita <kunihiro_narita@yahoo.co.jp>
2020-08-22 17:55:46 +0900

    ファイルをアーカイブか

---
+++ wbackup/CommandParser.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace wbackup
+{
+    class CommandParser
+    {
+        public string sourceDir;
+        public string destinationDir;
+
+        public CommandParser(string[] args)
+        {
+            bool isSource = false; ;
+            bool isDestination = false;
+
+            foreach (var arg in args)
+            {
+                if ('-' == arg.First() || '/' == arg.First())
+                {
+                    string command = arg.Substring(1);
+                    if ("srce" == command.ToLower())
+                    {
+                        isSource = true;
+                    }
+                    if ("dest" == command.ToLower())
+                    {
+                        isDestination = true;
+                    }
+                }
+                else
+                {
+                    if (isSource)
+                    {
+                        sourceDir = arg;
+                        isSource = false;
+                    }
+
+                    if (isDestination)
+                    {
+                        destinationDir = arg;
+                        isDestination = false;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ wbackup/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Enumeration;
+using System.Linq;
+using System.Threading;
+using System.Security.Cryptography.X509Certificates;
+using ICSharpCode.SharpZipLib.Zip;
+using System.Threading.Tasks;
+
+namespace wbackup
+{
+    class Program
+    {
+        public static CommandParser param;
+
+        static void Main(string[] args)
+        {
+            param = new CommandParser(args);
+            ConcurrentQueue<string> srceFilesQueue = new ConcurrentQueue<string>(Directory.GetFiles(param.sourceDir, "*.*", SearchOption.AllDirectories));
+
+            Parallel.For(0, 4, i => 
+            {
+                var nameTrans = new ZipNameTransform();
+                var zf = ZipFile.Create(param.destinationDir + "-" + DateTime.Now.ToString("yyyyMMddHHmmss") + "-" + i.ToString("00") + ".zip");
+                zf.BeginUpdate();
+
+                while (srceFilesQueue.TryDequeue(out string srceFileName))
+                {
+                    if (File.GetAttributes(srceFileName).HasFlag(FileAttributes.Archive))
+                    {
+                        string f = nameTrans.TransformFile(srceFileName);
+
+                        zf.Add(srceFileName, f);
+                    }
+                }
+
+                zf.CommitUpdate();
+            });
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
